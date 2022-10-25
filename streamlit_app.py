96f3f94e8da56d31fd780562b99dfb991f1c3a65
Baljit Singh <105224432+BaljitSingh55@users.noreply.github.com>
2022-10-25 09:00:22 -0700

    Update streamlit_app.py
    Fixed the errors

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,9 +27,9 @@ streamlit.dataframe(fruits_to_show)
 # New section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 try:
-    fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
     if not fruit_choice:
-      streamlit.write('The user entered ', fruit_choice)
+      streamlit.error("Please select a fruit tp get information.")
     else:
       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
       # take the json version of the response and normalize it
